838db75d4a01e829104e1a79e7c596dacdaf7adf
Ralu <ralucado@users.noreply.github.com>
2021-12-14 21:17:50 +0100

    cat is not feeling so good

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Cat : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public bool fromInside;
+    public Animator animator;
 
     private void Start()
     {
+        fromInside = true;
         rb = GetComponentInParent<Rigidbody2D>();
     }
 
@@ -20,11 +23,26 @@ public class Cat : MonoBehaviour
             rb.velocity = new Vector2(0, 0);
             rb.angularVelocity = 0;
             float xDir = someObject.gameObject.transform.position.x - rb.position.x;
-            rb.AddForce(new Vector2(-xDir* 220, 420));
+            rb.AddForce(new Vector2(-xDir * 220, 400));
             //Debug.DrawRay(rb.position, new Vector3(-xDir * 80, 320, 9), Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), 10f, false);
         }
+        else if (someObject.CompareTag("Inner"))
+            fromInside = true;
+        else if (someObject.CompareTag("Outer") && !fromInside)
+        {
+            Debug.DrawLine(someObject.transform.position, rb.position, Color.black, 20f, false);
+            Debug.Log("Scored a pointeroo!");
+            animator.SetBool("Saved", true);
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Inner"))
+            fromInside = false;
+    }
 
     // Update is called once per frame
     void Update()
