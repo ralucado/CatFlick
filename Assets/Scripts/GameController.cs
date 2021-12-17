2d6ce94e2c0d6f108007c38c1cd51dd2f9da57f0
Ralu <ralucado@users.noreply.github.com>
2021-12-17 01:35:38 +0100

    fixed something dont ask me what

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,10 +11,13 @@ public class GameController : MonoBehaviour
     public int catsDead;
     public GameObject catPrefab;
     private float lastTimeInterval;
+    private float lastQueueSpawnInterval;
     int extraCats;
     AudioSource audioSource;
     public AudioClip purr;
     public AudioClip die;
+    int catQueue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +27,9 @@ public class GameController : MonoBehaviour
         updateText(scoreText, catsSaved);
         updateText(dedText, catsDead);
         lastTimeInterval = Time.realtimeSinceStartup;
+        lastQueueSpawnInterval = Time.realtimeSinceStartup;
         extraCats = 0;
+        catQueue = 0;
     }
 
     private void updateText(Text text, int num)
@@ -36,20 +41,21 @@ public class GameController : MonoBehaviour
     {
         GameObject cat = Instantiate(catPrefab, new Vector3(Random.Range(-2.0f, 2.0f), 14, 0), Quaternion.identity);
         cat.BroadcastMessage("setGameController", gameObject);
+        --catQueue;
     }
 
     public void saveACat()
     {
         ++catsSaved;
         audioSource.PlayOneShot(purr, 1);
-        newCat();
+        ++catQueue;
     }
 
     public void killACat()
     {
         ++catsDead;
         audioSource.PlayOneShot(die, 0.3f);
-        newCat();
+        ++catQueue;
     }
 
     // Update is called once per frame
@@ -64,10 +70,15 @@ public class GameController : MonoBehaviour
             if(Time.realtimeSinceStartup - lastTimeInterval > 30)
             {
                 lastTimeInterval = Time.realtimeSinceStartup;
-                newCat();
+                ++catQueue;
                 ++extraCats;
             }
 
+        }
+        if (catQueue > 0 && Time.realtimeSinceStartup - lastQueueSpawnInterval >= 2)
+        {
+            newCat();
+            lastQueueSpawnInterval = Time.realtimeSinceStartup;
         }
     }
 
